9a63e05668e620fcd4c2f6125007137499a871da
Cédric Meuter <cedric.meuter@gmail.com>
2017-04-29 12:48:59 +0200

    Improved output using colors module

--- rollup-plugin-copy.js
+++ rollup-plugin-copy.js
@@ -1,14 +1,37 @@
 
 var fse = require("fs-extra");
+var colors = require("colors");
 
-function copy(options) {
-    const { src, dest } = options;
+function success(name, src, dest) {
+    console.log();
+    console.log(name + ": '" + src.green + "' -> '" + dest.green + "' (" +  "\u2714".green + ")");
+    console.log();    
+}
+
+function fatal(name, src, dest, err) {
+    console.error();
+    console.error(name + ": '" + src.red + "' -> '" + dest.red + "' (" +  "\u2718".red + ")");
+    console.error();
+    console.error("    " + err);
+    process.exit(-1);
+}
+
+function copy(options={}) {
+    const { 
+        src, 
+        dest, 
+        verbose=false } = options;
+    
+    const name = "rollup-plugin-copy";
+
     return {
-        name: "rollup-copyfile-plugin",
-        transform: function() { 
-            fs.copy(src, dest, err => {
-                if (err) return this.error(err)
-            })
+        name: name,
+        ongenerate: function(object) { 
+            fse.copy(src, dest).then( () => {
+                if (verbose) success(name, src, dest);
+            }).catch( (err) => {
+                fatal(name, src, dest, err);
+            });
         }
     }
 };
