1b0334cb0ac872ad4f460685c92a85619c0bb9d9
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2019-06-10 19:40:39 +0300

    Update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     'comma-dangle': ['error', 'never'],
+    'object-curly-newline': ['error', { multiline: true, consistent: true }],
     semi: ['error', 'never']
   },
   env: {
