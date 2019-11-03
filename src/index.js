21ef49e4d6768aa48617a8b22c12e2bcc77947ae
Blake Mealey <blakemealey@gmail.com>
2019-11-03 11:44:20 -0700

    Added initial file watching with Chokidar
    Author:    Blake Mealey <blakemealey@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import fs from 'fs-extra'
 import isObject from 'is-plain-object'
 import globby from 'globby'
 import { bold, green, yellow } from 'colorette'
+import chokidar from 'chokidar'
 
 function stringify(value) {
   return util.inspect(value, { breakLength: Infinity })
@@ -27,6 +28,76 @@ function generateCopyTarget(src, dest, rename) {
   }
 }
 
+function generateCopyTargets(src, dest, rename) {
+  return Array.isArray(dest)
+    ? dest.map(destination => generateCopyTarget(src, destination, rename))
+    : [generateCopyTarget(src, dest, rename)]
+}
+
+async function copyFiles(copyTargets, verbose, restPluginOptions) {
+  if (Array.isArray(copyTargets) && copyTargets.length) {
+    if (verbose) {
+      console.log(green('copied:'))
+    }
+
+    for (const { src, dest } of copyTargets) {
+      await fs.copy(src, dest, restPluginOptions)
+
+      if (verbose) {
+        console.log(green(`  ${bold(src)} → ${bold(dest)}`))
+      }
+    }
+  } else if (verbose) {
+    console.log(yellow('no items to copy'))
+  }
+}
+
+function watchFiles(targets, verbose, restPluginOptions) {
+  const watchers = targets.map((target) => {
+    const watcher = chokidar.watch(target.src, {
+      ignoreInitial: true
+    })
+
+    const { dest, rename } = target
+
+    async function onChange(matchedPath) {
+      console.log('file changed')
+      const copyTargets = generateCopyTargets(matchedPath, dest, rename)
+      await copyFiles(copyTargets, verbose, restPluginOptions)
+    }
+
+    watcher.on('change', onChange)
+    watcher.on('add', onChange)
+
+    return watcher
+  })
+
+  process.on('SIGINT', async () => {
+    console.log('CLOSE WATCHERS', watchers)
+    await Promise.all(watchers.forEach(watcher => watcher.close()))
+  })
+}
+
+function verifyTargets(targets) {
+  if (Array.isArray(targets) && targets.length) {
+    for (const target of targets) {
+      if (!isObject(target)) {
+        throw new Error(`${stringify(target)} target must be an object`)
+      }
+
+      const { src, dest, rename } = target
+
+      if (!src || !dest) {
+        throw new Error(`${stringify(target)} target must have "src" and "dest" properties`)
+      }
+
+      if (rename && typeof rename !== 'string' && typeof rename !== 'function') {
+        throw new Error(`${stringify(target)} target's "rename" property must be a string or a function`)
+      }
+    }
+  }
+}
+
 export default function copy(options = {}) {
   const {
     copyOnce = false,
@@ -37,6 +108,8 @@ export default function copy(options = {}) {
   } = options
 
   let copied = false
+
+  verifyTargets(targets)
 
   return {
     name: 'copy',
@@ -49,19 +122,7 @@ export default function copy(options = {}) {
 
       if (Array.isArray(targets) && targets.length) {
         for (const target of targets) {
-          if (!isObject(target)) {
-            throw new Error(`${stringify(target)} target must be an object`)
-          }
-
           const { src, dest, rename, ...restTargetOptions } = target
-
-          if (!src || !dest) {
-            throw new Error(`${stringify(target)} target must have "src" and "dest" properties`)
-          }
-
-          if (rename && typeof rename !== 'string' && typeof rename !== 'function') {
-            throw new Error(`${stringify(target)} target's "rename" property must be a string or a function`)
-          }
 
           const matchedPaths = await globby(src, {
             expandDirectories: false,
@@ -72,30 +133,17 @@ export default function copy(options = {}) {
 
           if (matchedPaths.length) {
             matchedPaths.forEach((matchedPath) => {
-              const generatedCopyTargets = Array.isArray(dest)
-                ? dest.map(destination => generateCopyTarget(matchedPath, destination, rename))
-                : [generateCopyTarget(matchedPath, dest, rename)]
-
-              copyTargets.push(...generatedCopyTargets)
+              copyTargets.push(...generateCopyTargets(matchedPath, dest, rename))
             })
           }
         }
       }
 
-      if (copyTargets.length) {
-        if (verbose) {
-          console.log(green('copied:'))
-        }
+      console.log('copying all files')
+      await copyFiles(copyTargets, verbose, restPluginOptions)
 
-        for (const { src, dest } of copyTargets) {
-          await fs.copy(src, dest, restPluginOptions)
-
-          if (verbose) {
-            console.log(green(`  ${bold(src)} → ${bold(dest)}`))
-          }
-        }
-      } else if (verbose) {
-        console.log(yellow('no items to copy'))
+      if (!copied && !copyOnce && process.env.ROLLUP_WATCH) {
+        watchFiles(targets, verbose, restPluginOptions)
       }
 
       copied = true
